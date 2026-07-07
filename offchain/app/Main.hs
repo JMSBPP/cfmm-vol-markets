3c5689c812190d602291c552426396deb94bd12c
JMSBPP <jmsbpp@hotmail.es>
2026-07-07 09:40:01 -0400

    fix: rpc handshake

--- offchain/app/Main.hs
+++ offchain/app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
+import Network.Web3.Provider
 import qualified Network.Ethereum.Api.Eth as GLOBAL_STATE
 
 main :: IO ()
-main = putStrLn "web3-ethereum is visible"
+main =
+  print =<< runWeb3' (HttpProvider "http://127.0.0.1:8545") GLOBAL_STATE.blockNumber
